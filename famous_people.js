137a463fec405fadd03486de72be4e637378b4b3
Alissa Balge <alissa.balge@gmail.com>
2018-10-16 17:10:46 +0000

    refactored function to export callback

--- famous_people.js
+++ famous_people.js
@@ -1,36 +1,13 @@
+const db = require("./test_script");
 
-const pg = require("pg");
-const settings = require("./settings"); // settings.json
-//acessing the object below
 var name = process.argv[2]
 
-const client = new pg.Client({
-  user     : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
-});
 
-//connecting to database
-
-client.connect((err) => {
-  if (err) {
-    return console.error("Connection Error", err);
-  }
-
-
- famousLookup(name);
- console.log("Searching....")
-});
-
-
-function famousLookup (name) {
-  client.query(`select first_name, last_name, birthdate::varchar from famous_people where first_name = $1::text`, [name], function (err, results) {
-    console.log("Found " + results.rows.length + " person(s) by the name " + name);
-    for (var i = 0; i < results.rows.length; i++) {
-      console.log((i + 1) + ": " + results.rows[i].first_name + " " + results.rows[i].last_name + ", born " + results.rows[i].birthdate);
+db.famousLookup (name, function (err, rows) {
+   console.log("Found " + rows.length + " person(s) by the name " + name);
+    for (var i = 0; i < rows.length; i++) {
+      console.log((i + 1) + ": " + rows[i].first_name + " " + rows[i].last_name + ", born " + rows[i].birthdate);
     }
+   db.close();
   });
-}
+
--- test_script.js
+++ test_script.js
@@ -1,27 +1,39 @@
+module.exports = (function () {
+
 const pg = require("pg");
 const settings = require("./settings"); // settings.json
 //acessing the object below
 
-const client = new pg.Client({
+const config = {
   user     : settings.user,
   password : settings.password,
   database : settings.database,
   host     : settings.hostname,
   port     : settings.port,
   ssl      : settings.ssl
-});
+};
+
+const client = new pg.Client(config);
 
 //connecting to database
 
-client.connect((err) => {
-  if (err) {
-    return console.error("Connection Error", err);
-  }
-  client.query("SELECT $1::int AS number", ["1"], (err, result) => {
-    if (err) {
-      return console.error("error running query", err);
-    }
-    console.log(result.rows[0].number); //output: 1
-    client.end();
+client.connect();
+
+function famousLookup (name, callback) {
+  client.query(`select first_name, last_name, birthdate::varchar from famous_people where first_name = $1::text`, [name], function (err, results) {
+    console.log("in here", results, err);
+  callback(err, results.rows);
   });
-});
+}
+
+function close() {
+    client.end();
+  };
+
+
+  return {
+    famousLookup: famousLookup,
+    close : close
+  }
+
+})();
