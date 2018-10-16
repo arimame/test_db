9da1483bf8f098684822d28d03efda5951d8f5b3
Alissa Balge <alissa.balge@gmail.com>
2018-10-16 16:37:32 +0000

    created famous people look up function

---
+++ test_script.js
@@ -0,0 +1,27 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+//acessing the object below
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+//connecting to database
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query("SELECT $1::int AS number", ["1"], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(result.rows[0].number); //output: 1
+    client.end();
+  });
+});
